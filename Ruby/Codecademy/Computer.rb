696a65fad1b01e68dc43d24043e9963540ce0c23
Navin Gunna <44994076+bonscotty@users.noreply.github.com>
2022-09-29 09:43:30 +0800

    Create Computer.rb

---
+++ Ruby/Codecademy/Computer.rb
@@ -0,0 +1,43 @@
+class Computer
+  @@users = {}
+
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @files = {}
+    @fileList = []
+    @@users[username] = password
+  end
+  
+  def create(filename)
+    time = Time.now
+    @files[filename] = time
+    puts "#{filename} was created at #{time} by #{@username}. "
+    @fileList.push(filename)
+  end
+  
+  def Computer.get_users
+    @@users
+  end
+
+  def printFiles()
+    @fileList
+  end
+end
+
+my_computer = Computer.new("Navin", 123456)
+her_computer = Computer.new("Kat", 42069)
+
+my_computer.create("Pornlinks")
+my_computer.create("Pornlinks2")
+
+her_computer.create("Kuromi")
+her_computer.create("MyMelody")
+
+puts Computer.get_users()
+
+puts my_computer.printFiles()
+puts my_computer.printFiles()
+
+puts her_computer.printFiles()
+puts her_computer.printFiles()
